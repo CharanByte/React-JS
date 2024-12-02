e086d2937478e57448eb3d2715c8ab00787f8d77
CharanByte <charan7812@gmail.com>
2024-12-02 21:34:21 +0530

    react forms

--- my-react-app/src/react forms/forms.js
+++ my-react-app/src/react forms/forms.js
@@ -5,20 +5,30 @@ function Forms(){
 
     const [inputText,setinputText]=useState("");
     const [updateText,setupdateText]=useState("");
+    const [renderColor,setRenderColor]=useState(false);
     function handelText(event){
         console.log(event.target.value);
         setinputText(event.target.value);
         
     }
 
+    function updateColor(){
+        setRenderColor(true);
+    }
+    function romoveColor(){
+        setRenderColor(false);
+    }
+
     function update(){
         setupdateText(inputText)
     }
 
+
     return(<div>
         <h1>hello {updateText}</h1>
         <input placeholder="Enter your name" onChange={handelText}></input><br></br>
-        <button onClick={update}>Submit</button>
+        <button style={{backgroundColor:renderColor?"green" :"white"}} onMouseOver={updateColor} 
+        onMouseOut={romoveColor} onClick={update}>Submit</button>
 
     </div>)
 }
